0dabb67e9a96758a8b4dacbbb6193e16e48d0670
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-01-15 13:48:38 +0530

    Inserting Multiple Documents using Mongoose

--- index.js
+++ index.js
@@ -23,16 +23,23 @@ const Employee = mongoose.model("Employee", userSchema);
 
 
 //Inserting documents in collection
-const user1 = new User({
-    name:"CG",
-    email:"cg@gmail.com",
-    age:20,
-});
-user1.save();
-const user2 = new User({
-    name:"SCG",
-    email:"scg@gmail.com",
-    age:20,
-});
-user2.save().then((res)=>console.log(res))
-.catch((err)=>console.log(err));
+// const user1 = new User({
+//     name:"CG",
+//     email:"cg@gmail.com",
+//     age:20,
+// });
+// user1.save();
+// const user2 = new User({
+//     name:"SCG",
+//     email:"scg@gmail.com",
+//     age:20,
+// });
+// user2.save().then((res)=>console.log(res))
+// .catch((err)=>console.log(err));
+
+//Inserting Multiple Documents using Mongoose
+User.insertMany([
+    {name:"Tony",email:"tony@gmail.com", age : 69},
+    {name:"Peter",email:"peter@gmail.com", age : 39},
+    {name:"Bruce",email:"Bruce@gmail.com", age : 29},
+]).then((res)=>console.log(res));
