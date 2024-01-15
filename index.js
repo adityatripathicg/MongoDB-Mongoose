558f7de27429f4b9b598fcd7044f43030c19ee37
Aditya Tripathi <adityatripathi3906@gmail.com>
2024-01-15 13:09:02 +0530

    //Till Inserting single documents in collection

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const mongoose = require('mongoose');
-
+//Building connection using mongoose
 main()
 .then(()=>{
     console.log("Connection Successful")
 })
 .catch((err)=>{console.log(err)});
 
-
 async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
+//creating Schema in Mongoose
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age : Number,
+});
+
+
+//creating model(collection) using mongoose
+const User = mongoose.model("User", userSchema);
+const Employee = mongoose.model("Employee", userSchema);
+
+
+//Inserting documents in collection
+const user1 = new User({
+    name:"CG",
+    email:"cg@gmail.com",
+    age:20,
+});
+user1.save();
+const user2 = new User({
+    name:"SCG",
+    email:"scg@gmail.com",
+    age:20,
+});
+user2.save().then((res)=>console.log(res))
+.catch((err)=>console.log(err));
